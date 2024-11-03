89403f81f0dc8c525401b1b1ced71a0216497943
SuperComputaria <kairos@computaria.org>
2024-11-03 10:21:47 -0300

    Small Blind Adjustments

--- rempointer.sh
+++ rempointer.sh
@@ -1,41 +1,48 @@
-#!/bin/bash
+#!/bin/sh
 
-#####################################################################
-#                                                                   #
-# Bate o ponto em https://www.dimepkairos.com.br.                   #
-# O usuário e senha podem ser passados como parâmetros ou definidos #
-# nas variáveis abaixo de forma estática.                           #
-#                                                                   #
-#####################################################################
+KAIROS_USER=''
+KAIROS_PASS=''
+KAIROS_DATE="$(date +"%d-%m-%Y %H:%M:00" | tr ' ' '+' | sed -e 's/:/%3A/g')"
+LOGDIR="${HOME}/mylogs"
+LOGFILE="${LOGDIR}/kairos.log"
 
-KAIROS_USER='EMAIL'
-KAIROS_PASS='SENHA NO KAIROS'
-KAIROS_DATE=$(date +"%d-%m-%Y %H:%M:00 - %A - OK")
+USER_AGENT='Mozilla/5.0 (X11; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0'
 
-function usage() {
-        echo -e 'Usage:\nkairos.sh -u <email> -p <password>' 1>&2
-        exit 1
+URL=https://www.dimepkairos.com.br/Dimep/Account/Marcacao
+COOKIE=kairos.cookie
+
+
+usage() {
+  printf 'Usage: %s -u <email> -p <password>\n' "${0}" >&2
+  exit 1
 }
 
 while getopts ":h:u:p:" o; do
-        case "${o}" in
-                u)
-                        KAIROS_USER=${OPTARG}
-                        ;;
-                p)
-                        KAIROS_PASS=${OPTARG}
-                        ;;
-                *)
-                        usage
-                        ;;
-        esac
+  case "${o}" in
+  u)
+    KAIROS_USER=${OPTARG}
+    ;;
+  p)
+    KAIROS_PASS=${OPTARG}
+    ;;
+  *)
+    usage
+    ;;
+  esac
 done
 
 if [ -z "$KAIROS_USER" ] || [ -z "$KAIROS_PASS" ]; then
-        usage
+  usage
 fi
 
-curl -sL --cookie-jar cookie 'https://www.dimepkairos.com.br/Dimep/Account/Marcacao'
-curl -sL --cookie-jar cookie -d "UserName=$KAIROS_USER" -d "Password=$KAIROS_PASS" -d "DateMarking=$KAIROS_DATE" 'https://www.dimepkairos.com.br/Dimep/Account/Marcacao'
-rm -f cookie
-echo "$KAIROS_DATE" >> $HOME/mylogs/kairos.log
+curl -sL -c "${COOKIE}" -e "${URL}" -A "${USER_AGENT}" "${URL}"
+
+curl -sL -b "${COOKIE}" -e "${URL}" -A "${USER_AGENT}" \
+     -d "UserName=${KAIROS_USER}" -d "Password=${KAIROS_PASS}" \
+     -d "DateMarking=${KAIROS_DATE}" -d "Ip=false" \
+     "${URL}"
+rm -f "${COOKIE}"
+
+mkdir -p "${LOGDIR}"
+echo "${KAIROS_DATE}" >> "${LOGFILE}"
+# vim:set ts=2 sw=2 et:
